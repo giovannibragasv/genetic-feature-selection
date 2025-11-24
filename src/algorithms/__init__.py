f35ad9e6f9ea27716308abe0ea1f1ed9af9d796b
giovannibragasv <giovannibragav@gmail.com>
2025-11-24 15:25:51 -0300

    feat: add initialization for algorithms and data modules

---
+++ src/algorithms/__init__.py
@@ -0,0 +1,10 @@
+from .knn_classifier import KNNClassifier, find_optimal_k
+from .genetic_algorithm import GeneticAlgorithm
+from .matrix_ga import MatrixGeneticAlgorithm
+
+__all__ = [
+    'KNNClassifier',
+    'find_optimal_k',
+    'GeneticAlgorithm',
+    'MatrixGeneticAlgorithm'
+]
---
+++ src/data/__init__.py
@@ -0,0 +1,8 @@
+from .loader import DataLoader
+from .preprocessor import DataPreprocessor, preprocess_dataset
+
+__all__ = [
+    'DataLoader',
+    'DataPreprocessor',
+    'preprocess_dataset'
+]
